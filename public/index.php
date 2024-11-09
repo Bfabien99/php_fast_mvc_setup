e34b8285fe729e12f6519ac6a3768028bb4d80a3
Brou kouadio Stéphane-Fabien <fabienbrou99@gmail.com>
2024-11-09 13:04:30 +0000

    CONSTANTS and .env load

--- public/index.php
+++ public/index.php
@@ -1,9 +1,10 @@
 <?php
-require_once  '../vendor/autoload.php';
 
+require_once  '../vendor/autoload.php';
 session_start();
 
 define('HOME_PATH', dirname(__DIR__));
 define('PUBLIC_PATH', HOME_PATH . DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR);
 define('VIEWS_PATH', HOME_PATH . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR);
 define('STORAGE_PATH', HOME_PATH . DIRECTORY_SEPARATOR . 'storage' . DIRECTORY_SEPARATOR);
+Dotenv\Dotenv::createUnsafeImmutable(HOME_PATH)->load();
