adf9a4acb74289a33db947315bb95a3d6ba44218
Brou kouadio Stéphane-Fabien <fabienbrou99@gmail.com>
2024-11-09 13:17:05 +0000

    ajout de altorouter

--- public/index.php
+++ public/index.php
@@ -1,10 +1,26 @@
 <?php
 
+use App\Helpers\View;
+
 require_once  '../vendor/autoload.php';
 session_start();
 
 define('HOME_PATH', dirname(__DIR__));
 define('PUBLIC_PATH', HOME_PATH . DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR);
-define('VIEWS_PATH', HOME_PATH . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR);
+define('RESOURCES_PATH', HOME_PATH . DIRECTORY_SEPARATOR . 'resources' . DIRECTORY_SEPARATOR);
+define('VIEWS_PATH', RESOURCES_PATH . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR);
 define('STORAGE_PATH', HOME_PATH . DIRECTORY_SEPARATOR . 'storage' . DIRECTORY_SEPARATOR);
 Dotenv\Dotenv::createUnsafeImmutable(HOME_PATH)->load();
+
+$router = new AltoRouter();
+$router->map( 'GET', '/', function() { echo "index"; }, 'home' );
+
+// assuming current request url = '/'
+$match = $router->match();
+if( is_array($match) && is_callable( $match['target'] ) ) {
+	call_user_func_array( $match['target'], $match['params'] );
+} else {
+	// no route was matched
+	http_response_code(404);
+    View::render('error/404');
+}
